2007a8c91b1722a491b4edb6eb1ce8a7cea873a2
Gabe <gabrieljameskeller@gmail.com>
2021-12-29 23:13:52 -0600

    first commit

---
+++ src/commands/boop.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment, MessageEmbed } = require('discord.js');
+
+const imageGenerator = require('../../imageGenerator.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('boop')
+		.setDescription('Boop!'),
+	async execute(interaction) {
+        await imageGenerator.generateImage()
+
+        const attachment = new MessageAttachment("background-copy.jpg");
+
+        await interaction.reply({files: [attachment] })
+        
+        setTimeout(() => {
+            const emojis = [":smiling_imp:", ":smirk:", ":scream:", ":face_with_symbols_over_mouth:", ":pleading_face:", ":lipstick:", ":rotating_light:", ":woman_running:"]
+
+            interaction.channel.send("<@501212640392118272> **b o o p** " + emojis[Math.floor(Math.random() * emojis.length)]).then(msg => {
+                setTimeout(() => {
+                    msg.delete()
+                }, 500)
+            })
+        }, 0);
+	},
+};
